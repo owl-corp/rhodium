f3d8430d9f602eff5c6738aaa6fb914bad5b6df8
Chris Lovering <chris.lovering.95@gmail.com>
2026-06-16 00:07:11 +0100

    Simplify phash implementation

--- src/phash.rs
+++ src/phash.rs
@@ -12,15 +12,11 @@ pub fn decode_image(bytes: &[u8]) -> Option<DynamicImage> {
 }
 
 pub fn phash(img: &DynamicImage) -> [u8; 8] {
-    let hasher = HasherConfig::with_bytes_type::<[u8; 8]>()
+    HasherConfig::with_bytes_type::<[u8; 8]>()
         .hash_size(8, 8)
         .hash_alg(HashAlg::Mean)
         .preproc_dct()
-        .to_hasher();
-
-    let hash = hasher.hash_image(img);
-
-    let mut out = [0u8; 8];
-    out.copy_from_slice(hash.as_bytes());
-    out
+        .to_hasher()
+        .hash_image(img)
+        .into_inner()
 }
