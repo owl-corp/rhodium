7fc0234c90579c53142a1d8f9695194392bab1f6
Chris Lovering <chris.lovering.95@gmail.com>
2026-06-16 01:05:10 +0100

    Simplify error handling

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,7 @@ mod phash;
 /// # Errors
 /// Returns an error if request handling fails at any stage.
 pub async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    let func_resp = handler::handle(req, env, ctx).await;
-
-    if let Err(err) = &func_resp {
-        return Response::error(err.to_string(), 500);
-    }
-
-    func_resp
+    handler::handle(req, env, ctx)
+        .await
+        .or_else(|err| Response::error(format!("{err:?}"), 500))
 }
